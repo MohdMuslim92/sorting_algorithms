9cb9d286509270204c570343d8382156bbb1ed24
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-22 22:44:03 +0000

    Update header file sort.h
    - Add quick_sort function prototype which sort an array with the help of
      :
        quicksort_recursion
        partition

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,4 +23,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quicksort_recursion(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high);
 #endif
