cd1c9aa9d42229509e7fef98c133ae6f7d30bcdc
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-06-23 00:04:51 +0000

    Update header file, add shell_sort function prototype

--- sort.h
+++ sort.h
@@ -27,4 +27,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort_recursion(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high);
+void shell_sort(int *array, size_t size);
 #endif
